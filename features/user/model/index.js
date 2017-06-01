5198d8e6be7a7a1abc5040e8647301268a963da0
Parth Mahajan <parthmahajan94@gmail.com>
2017-06-01 13:07:19 +0530

    added user model to feature

--- features/user/model/index.js
+++ features/user/model/index.js
@@ -4,7 +4,17 @@ let mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-
+	name: {
+		first: {type: String},
+		last: {type: String},
+		middle: {type: String}
+	}, 
+	contactInfo: {
+		email: {type: String, unique: true},
+		phoneNumber: {type: Number},
+		address: {type: String}
+	}, 
+	rollNumber: {type: Number, unique: true}
 });
 
 mongoose.model('User', userSchema);
